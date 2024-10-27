adf2ba1d4c31884269bddc968545e49866cb8005
Saswat Samal <saswat1091@outlook.com>
2024-10-27 10:26:39 +0530

    update AddEditNoteScreen.kt

---
+++ app/src/main/java/com/ibml/noteit/feature_note/presentation/add_edit_note/AddEditNoteScreen.kt
@@ -0,0 +1,169 @@
+package com.ibml.noteit.feature_note.presentation.add_edit_note
+
+import androidx.compose.animation.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.ViewModel
+import androidx.navigation.NavController
+import androidx.room.util.TableInfo.Column
+import com.ibml.noteit.feature_note.domain.model.Note
+import com.ibml.noteit.feature_note.presentation.add_edit_note.components.TransparentTextField
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
+@Composable
+fun AddEditNoteScreen(
+    navController: NavController,
+    noteColor: Int,
+    viewModel: AddEditNoteViewModel = hiltViewModel()
+) {
+    val titleState = viewModel.noteTitle
+    val contentState = viewModel.noteContent
+    val noteBackgroundAnimatable = remember {
+        Animatable(
+            Color(if (noteColor != -1) noteColor else viewModel.noteColor.value)
+        )
+    }
+
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+
+
+    LaunchedEffect(key1 = true) {
+        viewModel.eventFlow.collectLatest {event->
+            when(event){
+                is AddEditNoteViewModel.UiEvent.ShowSnackbar -> {
+                    snackbarHostState.showSnackbar(
+                        message = event.message
+                    )
+                }
+                is AddEditNoteViewModel.UiEvent.SaveNote -> {
+                    navController.navigateUp()
+                }
+            }
+        }
+    }
+
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { viewModel.onEvent(AddEditNoteEvent.SaveNote) },
+                modifier = Modifier.background(MaterialTheme.colorScheme.primaryContainer),
+                content = {
+                    Icon(imageVector = Icons.Filled.CheckCircle, contentDescription = "Save Note")
+                }
+            )
+        },
+        snackbarHost = { SnackbarHost(snackbarHostState) },
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(it)
+                .fillMaxSize()
+                .background(noteBackgroundAnimatable.value)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Note.noteColors.forEach { color ->
+                    val ColorInt = color.toArgb()
+                    Box(
+                        modifier = Modifier
+                            .size(50.dp)
+                            .clip(CircleShape)
+                            .background(color)
+                            .border(
+                                width = 3.dp,
+                                color = if (viewModel.noteColor.value == ColorInt) {
+                                    Color.Black
+                                } else {
+                                    Color.Transparent
+                                },
+                                shape = CircleShape
+                            )
+                            .clickable {
+                                scope.launch {
+                                    noteBackgroundAnimatable.animateTo(
+                                        targetValue = Color(ColorInt),
+                                        animationSpec = tween(
+                                            500
+                                        )
+                                    )
+                                }
+                                viewModel.onEvent(AddEditNoteEvent.ChangeColor(ColorInt))
+                            }
+                    )
+                }
+            }
+            Spacer(Modifier.height(8.dp))
+
+            TransparentTextField(
+                text = titleState.value.text,
+                hint = titleState.value.hint,
+                isHintVisible = titleState.value.isHintVisible,
+                onValChange = {
+                    viewModel.onEvent(AddEditNoteEvent.EnteredTitle(it))
+                },
+                singleLine = true,
+                onFocusChange = {
+                    viewModel.onEvent(AddEditNoteEvent.ChangeTitleFocus(it))
+                },
+                textStyle = MaterialTheme.typography.titleLarge
+            )
+
+            Spacer(Modifier.height(16.dp))
+
+            TransparentTextField(
+                text = contentState.value.text,
+                hint = contentState.value.hint,
+                isHintVisible = contentState.value.isHintVisible,
+                onValChange = {
+                    viewModel.onEvent(AddEditNoteEvent.EnteredContent(it))
+                },
+                singleLine = false,
+                onFocusChange = {
+                    viewModel.onEvent(AddEditNoteEvent.ChangeContentFocus(it))
+                },
+                textStyle = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.fillMaxHeight()
+            )
+        }
+    }
+
+
+}
--- app/src/main/java/com/ibml/noteit/feature_note/presentation/add_edit_note/components/TransparentTextField.kt
+++ app/src/main/java/com/ibml/noteit/feature_note/presentation/add_edit_note/components/TransparentTextField.kt
@@ -1,17 +1,39 @@
 package com.ibml.noteit.feature_note.presentation.add_edit_note.components
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusState
+import androidx.compose.ui.focus.onFocusEvent
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 
 @Composable
 fun TransparentTextField(
     modifier:Modifier = Modifier,
     text: String,
+    textStyle: TextStyle = TextStyle(),
     hint: String,
     isHintVisible: Boolean= true,
     onValChange: (String) -> Unit,
     singleLine: Boolean = false,
     onFocusChange: (FocusState) -> Unit
 ) {
+    Box(modifier= modifier ){
+        BasicTextField(
+            value = text,
+            onValueChange = onValChange,
+            singleLine= singleLine,
+            textStyle = textStyle,
+            modifier = Modifier
+                .fillMaxWidth()
+                .onFocusEvent{onFocusChange(it)}
+        )
+        if(isHintVisible){
+            Text(text = hint, style = textStyle, color = Color.DarkGray)
+        }
+    }
 }
